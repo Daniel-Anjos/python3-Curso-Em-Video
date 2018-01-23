099d6d28bf2f8743b00e8415893029d15a6da40e
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2018-01-23 21:59:42 -0200

    Primeira atualização
    Repetindo até o jogador perder

--- ex068.py
+++ ex068.py
@@ -3,32 +3,28 @@ O programa deve também mostrar a quantidade de vitórias consecutivas do jogado
 
 from random import randint
 
-print('=-'*20)
+print('=-' * 20)
 print('Jogo do Par ou Ímpar')
-print('=-'*20)
+print('=-' * 20)
 
-num = int(input('Digite um valor: '))
-user = str(input('Par ou Ímpar? [P/I]').strip().upper() [0])
-print('-'*30)
+while True:
+    num = int(input('Digite um valor: '))
+    user = str(input('Par ou Ímpar? [P/I]: ').strip().upper()[0])
+    print('-' * 30)
+    x = randint(0, 10)
+    total = x + num
 
-x = randint (0,10)
-total = x + num
-
-if (total % 2 == 0):
-   win = 'P'
-   y = 'Par'
-else:
-   win = 'I'
-   y = 'Impar'
-   
-if win == user:
-   print (f'Você jogou {num} e o computador {x}. O total foi {total} que é {y}')
-   print('Você ganhou!')
-else: 
-   print(f'Você jogou {num} e o computador {x}. O total foi {total} que é {y}')
-   print('Você perdeu')
-
-
-#print(f'Você jogou {num} e o computador {x}. Deu empate. ')
-
-
+    if (total % 2 == 0):
+        win = 'P'
+        y = 'Par'
+    else:
+        win = 'I'
+        y = 'Ímpar'
+    if win == user:
+        print(f'Você jogou {num} e o computador {x}. O total foi {total} que é {y}')
+        print('Você ganhou!')
+        print('Vamos jogar novamente... \n')
+    else:
+        print(f'Você jogou {num} e o computador {x}. O total foi {total} que é {y}')
+        print('Você perdeu')
+        break
