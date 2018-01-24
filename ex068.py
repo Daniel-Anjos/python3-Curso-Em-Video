7110e404f583d907506b4e7d1f23fe48b97e1590
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2018-01-23 22:21:07 -0200

    Update ex068
    Adicionada verificação para se o cidadão informar um valor diferente de p ou i no par ou ímpar.
    Ele também considera a palavra toda, pois verifica apenas a primeira letra.

--- ex068.py
+++ ex068.py
@@ -3,13 +3,16 @@ O programa deve também mostrar a quantidade de vitórias consecutivas do jogado
 
 from random import randint
 vitorias = 0
+user = ' '
+
 print('=-' * 20)
 print('Jogo do Par ou Ímpar')
 print('=-' * 20)
 
 while True:
     num = int(input('Digite um valor: '))
-    user = str(input('Par ou Ímpar? [P/I]: ').strip().upper()[0])
+    while user not in 'PI':
+        user = str(input('Par ou Ímpar? [P/I]: ').strip().upper()[0])
     print('-' * 30)
     x = randint(0, 10)
     total = x + num
