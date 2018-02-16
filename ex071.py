527263313024e0beda0485d38d8015cf7510d77c
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2018-02-16 13:48:42 -0200

    Update
    Informando o valor ele divide entre a quantidade de notas. Falta uma última revisão para verificar a possibilidade de otimização.

--- ex071.py
+++ ex071.py
@@ -6,14 +6,24 @@ OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
 
 valor = int(input('Informe o valor do saque: R$'))
 resto = valor
+sobra = 0
 
-while True:
+notas50 = valor // 50
+  resto = valor % 50
+  sobra = valor - (notas50 * 50)
+  notas20 = (sobra // 20)
+  resto = sobra % 20
+  sobra = sobra - (notas20 * 20)
+  notas10 = (sobra //10)
+  resto = sobra % 10
+  sobra = sobra - (notas10 * 10)
+  notas1 = (sobra // 1)
+  resto = sobra % 1
   if resto == 0:
-     break
-  else:
-    notas50 = (valor / 50)
-    resto = valor % 50
-    notas20 = (resto / 20)
-    resto =  resto % 20
-    notas1 = resto / 1
-    resto = resto % 1
+    break
+
+print('Você receberá: ')
+print(f'{notas50:.0f} notas de R$50,00')
+print(f'{notas20} notas de R$20,00')
+print(f'{notas10} notas de R$10,00')
+print(f'{notas1} notas de R$1,00')
